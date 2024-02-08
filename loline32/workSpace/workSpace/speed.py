e6e881636a7a7a6bb0ec88243dc21dfeb60a76a5
matherm <matze_hah@t-online.de>
2024-02-08 23:10:26 +0100

    Update speed.py

--- loline32/workSpace/workSpace/speed.py
+++ loline32/workSpace/workSpace/speed.py
@@ -1,13 +1,25 @@
 import time, gc, os
 from ulab import numpy as np
+from machine import PIN
+
+
+digi = Pin(33  , Pin.IN)
+pot = ADC(Pin(32))
+pot.atten(ADC.ATTN_11DB) 
+pot.width(ADC.WIDTH_11BIT)
 
 TIMING = [0] * 50
+vals = [0] * 50
 ticks = 0
 while True:
     ticks = ticks + 1
     TIMING[ticks % len(TIMING)] = time.ticks_us()
+    vals[ticks % len(TIMING)] = pot.read()
     
     if time.ticks_us() - ticks >= 0: 
-      pass
+        pass
+
+    if ticks > 100000:
+        break
      
 a = 1000*1000*len(TIMING)/((np.max(TIMING) - np.min(TIMING)))
